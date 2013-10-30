b8c3dd74b0874f010bef69b42b249aa97af7f743
jakenjarvis <jaken.jarvis@gmail.com>
2013-10-30 13:27:23 +0900

    Deleted CodeGen.java

--- ormlite-content-provider-compiler/src/main/java/com/tojc/ormlite/android/compiler/ContractAnnotationProcessor.java
+++ ormlite-content-provider-compiler/src/main/java/com/tojc/ormlite/android/compiler/ContractAnnotationProcessor.java
@@ -29,17 +29,25 @@ import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.tools.Diagnostic.Kind;
 import javax.tools.JavaFileObject;
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.EnumSet;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeSet;
 
 import static javax.lang.model.element.Modifier.FINAL;
 import static javax.lang.model.element.Modifier.PRIVATE;
@@ -69,7 +77,7 @@ public class ContractAnnotationProcessor extends AbstractProcessor {
         final Set<? extends Element> annotatedElements = roundEnvironment.getElementsAnnotatedWith(Contract.class);
 
         // Get the annotation information grouped by ContractClassName
-        final Map<String, Set<Element>> grouped = CodeGen.groupElementsByContractClassName(annotatedElements);
+        final Map<String, Set<Element>> grouped = groupElementsByContractClassName(annotatedElements);
 
         // process every ContractClassName
         for (final Map.Entry<String, Set<Element>> groupedElements : grouped.entrySet()) {
@@ -178,7 +186,7 @@ public class ContractAnnotationProcessor extends AbstractProcessor {
                             .endMethod()
                             .emitEmptyLine();
 
-                    final List<Element> fields = CodeGen.getAllElementsAnnotatedWith(DatabaseField.class, classElement);
+                    final List<Element> fields = getAllElementsAnnotatedWith(DatabaseField.class, classElement);
                     for (final Element field : fields) {
                         final String fieldName = field.getSimpleName().toString();
                         if (!("_id".equals(fieldName) || "_id".equals(field.getAnnotation(DatabaseField.class).columnName()))) {
@@ -212,4 +220,78 @@ public class ContractAnnotationProcessor extends AbstractProcessor {
         }
         return true;
     }
+
+
+    private static final Comparator<Element> ELEMENT_COMPARATOR = new Comparator<Element>() {
+        @Override
+        public int compare(final Element o1, final Element o2) {
+            if (o1 != null) {
+                final String className1 = o1.getSimpleName().toString();
+                final String className2 = o2.getSimpleName().toString();
+                return className1.compareTo(className2);
+            } else {
+                if (o2 == null) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            }
+        }
+    };
+
+    public PackageElement getPackage(Element type) {
+        while (type.getKind() != ElementKind.PACKAGE) {
+            type = type.getEnclosingElement();
+        }
+        return (PackageElement) type;
+    }
+
+    public List<Element> getAllElementsAnnotatedWith(final Class<? extends Annotation> class1, final Element classElement) {
+        final List<Element> allFields = getEnclosedElements(classElement, ElementKind.FIELD);
+        final List<Element> result = new ArrayList<Element>(allFields.size());
+        for (final Element fieldElement : allFields) {
+            if (fieldElement.getAnnotation(class1) != null) {
+                result.add(fieldElement);
+            }
+        }
+        return result;
+    }
+
+    public List<Element> getEnclosedElements(final Element element, final ElementKind elementKind) {
+        final List<? extends Element> enclosedElements = element.getEnclosedElements();
+        final List<Element> list = new ArrayList<Element>(enclosedElements.size());
+        for (final Element enclosedElement : enclosedElements) {
+            if (enclosedElement.getKind() == elementKind) {
+                list.add(enclosedElement);
+            }
+        }
+        return list;
+    }
+
+    public Map<String, Set<Element>> groupElementsByContractClassName(final Collection<? extends Element> classElements) {
+        final Map<String, Set<Element>> grouped = new HashMap<String, Set<Element>>(classElements.size());
+        for (final Element classElement : classElements) {
+            final Contract contractAnnotation = classElement.getAnnotation(Contract.class);
+            final String contractClassName = contractAnnotation.contractClassName();
+
+            final String targetClassName;
+            if (contractClassName == null || contractClassName.isEmpty()) {
+                final PackageElement targetPackage;
+                targetPackage = getPackage(classElement);
+                targetClassName = targetPackage.getQualifiedName().toString() + '.' + classElement.getSimpleName() + "Contract";
+            } else {
+                targetClassName = contractClassName;
+            }
+
+            if (grouped.containsKey(targetClassName)) {
+                grouped.get(targetClassName).add(classElement);
+            } else {
+                final Set<Element> elements = new TreeSet<Element>(ELEMENT_COMPARATOR);
+                elements.add(classElement);
+                grouped.put(targetClassName, elements);
+            }
+        }
+        return grouped;
+    }
+
 }
