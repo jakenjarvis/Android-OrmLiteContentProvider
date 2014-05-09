7d486f55082812d45a018a78088e202d439a96fb
jakenjarvis <jaken.jarvis@gmail.com>
2014-05-10 04:59:27 +0900

    Copied the project to fragment-sample from sample.

---
+++ ormlite-content-provider-fragment-sample/src/com/tojc/ormlite/android/ormlitecontentprovider/fragment/sample/provider/SampleProvider.java
@@ -0,0 +1,42 @@
+/*
+ * This file is part of the Android-OrmLiteContentProvider package.
+ *
+ * Copyright (c) 2012, Android-OrmLiteContentProvider Team.
+ *                     Jaken Jarvis (jaken.jarvis@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The author may be contacted via
+ * https://github.com/jakenjarvis/Android-OrmLiteContentProvider
+ */
+package com.tojc.ormlite.android.ormlitecontentprovider.fragment.sample.provider;
+
+import com.tojc.ormlite.android.OrmLiteSimpleContentProvider;
+import com.tojc.ormlite.android.framework.MatcherController;
+import com.tojc.ormlite.android.framework.MimeTypeVnd.SubType;
+import com.tojc.ormlite.android.ormlitecontentprovider.fragment.sample.model.Account;
+
+public class SampleProvider extends OrmLiteSimpleContentProvider<SampleHelper> {
+    @Override
+    protected Class<SampleHelper> getHelperClass() {
+        return SampleHelper.class;
+    }
+
+    @Override
+    public boolean onCreate() {
+        setMatcherController(new MatcherController()//
+                .add(Account.class, SubType.DIRECTORY, "", AccountContract.CONTENT_URI_PATTERN_MANY)//
+                .add(Account.class, SubType.ITEM, "#", AccountContract.CONTENT_URI_PATTERN_ONE));
+        return true;
+    }
+}
